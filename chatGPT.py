9ff95065d9cb2eea1b0bf4d6a90008de19271f29
Brokyeom <goindol40@gmail.com>
2023-05-31 05:48:36 +0900

    feat : gradio 를 사용한 웹뷰 구현

--- chatGPT.py
+++ chatGPT.py
@@ -1,3 +1,4 @@
+import gradio as gr
 import openai
 import os
 from dotenv import load_dotenv
@@ -6,24 +7,34 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-temp = 20
+def chat_with_gpt(input_text):
+    temp = 25
+    messages = [
+        { "role": "system", "content": "오늘 기온에 맞춰 오늘 작물에게 맞는 행동을 추천해야 합니다. 사용자가 특정 작물과 현재 기온에 대한 정보를 제공하면, 해당 기온에서의 작물 관리에 대한 오늘의 관리법을 제공해야 합니다. 답변은 다음과 같은 형식을 따라야 합니다: '{temp}도에서는 {input}에게 ~~ 한 온도입니다. 다음과 같이 행동하는 것을 추천합니다: 1. ~~ 2. ~~ 3. ~~'"  },
+        {"role": "assistant", "content": f'{temp}도에서는 {input_text}에게 ~~ 한 온도입니다. 다음과 같이 행동하는 것을 추천합니다: 1. ~~ 2. ~~ 3. ~~'},
+        {"role": "assistant", "content": f'{temp}도에서는 {input_text}에게 ~~ 한 온도입니다. 다음과 같이 행동하는 것을 추천합니다: 1. ~~ 2. ~~ 3. ~~'},
+        {"role": "assistant", "content": f'{temp}도에서는 {input_text}에게 ~~ 한 온도입니다. 다음과 같이 행동하는 것을 추천합니다: 1. ~~ 2. ~~ 3. ~~'},
+        {"role": "assistant", "content": f'{temp}도에서는 {input_text}에게 ~~ 한 온도입니다. 다음과 같이 행동하는 것을 추천합니다: 1. ~~ 2. ~~ 3. ~~'},
+        {"role": "assistant", "content": f'{temp}도에서는 {input_text}에게 ~~ 한 온도입니다. 다음과 같이 행동하는 것을 추천합니다: 1. ~~ 2. ~~ 3. ~~'},
+        {"role": "user", "content": f'나는 {input_text} 를 기르고 있어. 오늘 기온은 섭씨{temp}도야 오늘 {input_text}가 잘 자라게 하는 방법에는 무엇이 있을까?'}
+    ]
 
-input_plant = input("확인하고자 하는 작물을 입력하세요 : ")
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.1
+    )
 
-messages = [
-    {"role": "assistant", "content": "반드시 다음과 같은 형식으로만 대답해줘. \n" +
-                                      f"현재 기온은 {temp}도 입니다. \n" +
-                                      f"{temp}도에서 {input_plant}의 최적 생육을 위해 아래와 같은 행동을 취하세요.\n"},
-    {"role": "user", "content": input_plant}
-]
+    assistant_content = completion.choices[0].message['content'].strip()
 
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages,
-    temperature= 0.2
+    return assistant_content
+
+iface = gr.Interface(
+    fn=chat_with_gpt, 
+    inputs="text",
+    outputs="text", 
+    title="AgriChat",
 )
 
-assistant_content = completion.choices[0].message['content'].strip()
-
-print('답변 : ', assistant_content)
+iface.launch()
 
