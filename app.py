a84a327674393e3f0d1b80cfc881d34e99c88259
Brokyeom <goindol40@gmail.com>
2023-06-04 14:29:09 +0900

    fix : 500 에러 해결

--- app.py
+++ app.py
@@ -11,13 +11,19 @@ app = Flask(__name__)
 API_KEY = os.getenv("WEATHER_SERVICE_KEY")
 
 @app.route('/', methods=['GET'])
-def get_weather(lat, long):
+def get_weather():
+    lat = request.args.get('lat')
+    long = request.args.get('long')
+
+    if not lat or not long:
+        return "Missing latitude and/or longitude", 400
+
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
     temp = data['main']['temp']
 
-    return temp
+    return str(temp)
 
 def get_location():
     try:
