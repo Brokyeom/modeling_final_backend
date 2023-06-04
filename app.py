f723cb0dc78470f42176ebb1810cde49d3aba9c3
Brokyeom <goindol40@gmail.com>
2023-06-04 14:23:07 +0900

    feat : 기온 불러오기 기능 추가

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, request
+import openai
+import os
+import geocoder
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
+API_KEY = os.getenv("WEATHER_SERVICE_KEY")
 
 @app.route('/', methods=['GET'])
-def greet():
-    name = request.args.get('name', 'Guest')  # 'name'이라는 쿼리 파라미터를 가져옵니다. 기본값은 'Guest'입니다.
-    return f'Hello, {name}!'
+def get_weather(lat, long):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    temp = data['main']['temp']
+
+    return temp
+
+def get_location():
+    try:
+        g = geocoder.ip('me')
+        return g.lat, g.lng
+    except Exception as e:
+        print("Can't find you. No weather for you.")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
