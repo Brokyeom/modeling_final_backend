b726ec97c607d1163309a886a84ed608f6596a87
Brokyeom <goindol40@gmail.com>
2023-06-04 14:36:04 +0900

    fix : 위치 가져오기 기능 수정

--- app.py
+++ app.py
@@ -11,26 +11,24 @@ app = Flask(__name__)
 API_KEY = os.getenv("WEATHER_SERVICE_KEY")
 
 @app.route('/', methods=['GET'])
-def get_weather():
-    lat = request.args.get('lat')
-    long = request.args.get('long')
-
-    if not lat or not long:
-        return "Missing latitude and/or longitude", 400
-
+def get_weather(lat, long):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={long}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
+    city = data['name']
+    weather = data['weather'][0]['main']
     temp = data['main']['temp']
 
-    return str(temp)
+    return f'{city}의 날씨는 {weather}, 기온은 {temp}도 입니다.'
 
 def get_location():
     try:
         g = geocoder.ip('me')
-        return g.lat, g.lng
+        return get_weather(g.lat, g.lng)
     except Exception as e:
         print("Can't find you. No weather for you.")
+
+get_location()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
